82e48d3fd16d6b775ed655ae71a23e7c2e5e61ab
yingxingheiditan <yxhtan001@mymail.sim.edu.sg>
2025-09-15 21:13:06 +0800

    initial files setup

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # urban-threads-shop
 ## Set up
-# HelloWorld
 make venv then install requirements.txt:
 python3 -m venv proj_env
+
 pip install -r requirements.txt
+## How to run flask project
+
+flask --app main run
--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,12 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the .env file (if present)
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+    return app
